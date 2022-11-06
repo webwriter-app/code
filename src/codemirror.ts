d822713eacfa7b5521834cc8a763f28d525d257b
Erik <erik@MacBook-Pro-von-Erik.local>
2022-11-06 15:03:23 +0100

    added intend with tab

--- src/codemirror.ts
+++ src/codemirror.ts
@@ -2,7 +2,7 @@ import { lineNumbers, highlightActiveLineGutter, highlightSpecialChars, drawSele
 export { EditorView } from '@codemirror/view';
 import { EditorState } from '@codemirror/state';
 import { foldGutter, indentOnInput, syntaxHighlighting, defaultHighlightStyle, bracketMatching, foldKeymap } from '@codemirror/language';
-import { history, defaultKeymap, historyKeymap } from '@codemirror/commands';
+import { history, defaultKeymap, historyKeymap, indentWithTab } from '@codemirror/commands';
 import { highlightSelectionMatches, searchKeymap } from '@codemirror/search';
 import { closeBrackets, closeBracketsKeymap, completionKeymap } from '@codemirror/autocomplete';
 import { lintKeymap } from '@codemirror/lint';
@@ -39,7 +39,8 @@ export const mySetup = /*@__PURE__*/(() => [
         ...historyKeymap,
         ...foldKeymap,
         ...completionKeymap,
-        ...lintKeymap
+        ...lintKeymap,
+        indentWithTab
     ])
 ])();
 
