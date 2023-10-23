5825f30affd7495e88c614b27ee11c0562632250
Jorik van Genuchten <jorik@vangenuchten.de>
2023-10-23 21:43:39 +0200

    global / local executing

--- src/languageModules/javascriptModule.ts
+++ src/languageModules/javascriptModule.ts
@@ -5,64 +5,75 @@ import Code from '../ww-code';
 // capture console calls
 // Redirect results to code cell output
 
-
 const executeJavascript = (code: string, context: Code) => {
-  const oldConsole = window.console
-  window.console = {
-    log: (...objs: any[]) => {
-      objs.forEach(obj => context.results.push(String(obj)))
-    },
-    info: (...objs: any[]) => {
-      objs.forEach(obj => context.results.push(String(obj)))
-    },
-    warn: (...objs: any[]) => {
-      objs.forEach(obj => context.results.push(String(obj)))
-    },
-    error: (...objs: any[]) => {
-      objs.forEach(obj => context.results.push(String(obj)))
-    },
-    trace: (...objs: any[]) => {
-      objs.forEach(obj => context.results.push(String(obj)))
-    },
-    table: (...objs: any[]) => {
-      objs.forEach(obj => context.results.push(String(obj)))
-    },
-    assert: (assertion: boolean, ...objs: any[]) => {
-      assertion && objs.forEach(obj => context.results.push(String(obj)))
-    },
-    clear: () => {},
-    count: () => {},
-    countReset: () => {},
-    debug: (...objs: any[]) => {
-      objs.forEach(obj => context.results.push(String(obj)))
-    },
-    dir: () => {},
-    dirxml: () => {},
-    group: () => {},
-    groupCollapsed: () => {},
-    groupEnd: () => {},
-    profile: () => {},
-    profileEnd: () => {},
-    time: () => {},
-    timeEnd: () => {},
-    timeLog: () => {},
-    timeStamp: () => {},
-    Console: window.console.Console
-  }
-  try {
-    const result = scopedEval(code, context)
-    context.results.push(result)
-    return result
-  } catch (e) {
-      context.results.push(String(e))
-      return e;
-  } finally {
-    window.console = oldConsole
-  }
-}
+    const oldConsole = window.console;
+    window.console = {
+        log: (...objs: any[]) => {
+            objs.forEach((obj) => context.results.push({ color: 'inherit', text: String(obj) }));
+        },
+        info: (...objs: any[]) => {
+            objs.forEach((obj) => context.results.push({ color: 'inherit', text: String(obj) }));
+        },
+        warn: (...objs: any[]) => {
+            objs.forEach((obj) => context.results.push({ color: 'orange', text: String(obj) }));
+        },
+        error: (...objs: any[]) => {
+            objs.forEach((obj) => context.results.push({ color: 'red', text: String(obj) }));
+        },
+        trace: (...objs: any[]) => {
+            objs.forEach((obj) => context.results.push({ color: 'inherit', text: String(obj) }));
+        },
+        table: (...objs: any[]) => {
+            objs.forEach((obj) => context.results.push({ color: 'inherit', text: String(obj) }));
+        },
+        assert: (assertion: boolean, ...objs: any[]) => {
+            assertion && objs.forEach((obj) => context.results.push({ color: 'inherit', text: String(obj) }));
+        },
+        clear: () => {},
+        count: () => {},
+        countReset: () => {},
+        debug: (...objs: any[]) => {
+            objs.forEach((obj) => context.results.push({ color: 'inherit', text: String(obj) }));
+        },
+        dir: () => {},
+        dirxml: () => {},
+        group: () => {},
+        groupCollapsed: () => {},
+        groupEnd: () => {},
+        profile: () => {},
+        profileEnd: () => {},
+        time: () => {},
+        timeEnd: () => {},
+        timeLog: () => {},
+        timeStamp: () => {},
+        Console: window.console.Console,
+    };
+    try {
+        const result = context.globalExecution ? unScopeEval(code) : scopedEval(code, context);
+        context.results.push({ color: 'inherit', text: result });
+        return result;
+    } catch (e) {
+        context.results.push({ color: 'red', text: e.message });
+        return e;
+    } finally {
+        window.console = oldConsole;
+    }
+
+    context.results = [...context.results];
+};
 
 function scopedEval(code: string, context: Code) {
-    return Function(code)(context);
+    return Function(`
+      const codeToExecute = [${code
+          .split('\n')
+          .map((line) => `"${line}"`)
+          .join(',')}];
+      return eval(codeToExecute.join("\\n"));
+    `)();
+}
+
+function unScopeEval(code: string) {
+    return eval(code);
 }
 
 export const javascriptModule = {
--- src/ww-code.ts
+++ src/ww-code.ts
@@ -4,7 +4,7 @@ import { property, customElement, query } from 'lit/decorators.js';
 import { EditorView } from '@codemirror/view';
 import { EditorState, Compartment } from '@codemirror/state';
 import { style } from './ww-code-css';
-import { html } from 'lit';
+import { PropertyValueMap, html } from 'lit';
 import { basicSetup } from './codemirror-setup';
 import { autocompletion } from '@codemirror/autocomplete';
 import { highlightSelection } from './highlight';
@@ -15,55 +15,56 @@ import { javascriptModule } from './languageModules/javascriptModule';
 // import { pythonModule } from './languageModules/pythonModule';
 import { htmlModule } from './languageModules/htmlModule';
 import { cssModule } from './languageModules/cssModule';
-import SlButton from "@shoelace-style/shoelace/dist/components/button/button.js"
-import SlCard from "@shoelace-style/shoelace/dist/components/card/card.js"
-import SlCheckbox from "@shoelace-style/shoelace/dist/components/checkbox/checkbox.js"
-import SlDivider from "@shoelace-style/shoelace/dist/components/divider/divider.js"
-import SlSelect from "@shoelace-style/shoelace/dist/components/select/select.js"
-import SlIcon from "@shoelace-style/shoelace/dist/components/icon/icon.js"
-import SlMenu from "@shoelace-style/shoelace/dist/components/menu/menu.js"
-import SlMenuItem from "@shoelace-style/shoelace/dist/components/menu-item/menu-item.js"
-import SlSwitch from "@shoelace-style/shoelace/dist/components/switch/switch.js"
+import SlButton from '@shoelace-style/shoelace/dist/components/button/button.js';
+import SlCard from '@shoelace-style/shoelace/dist/components/card/card.js';
+import SlCheckbox from '@shoelace-style/shoelace/dist/components/checkbox/checkbox.js';
+import SlDivider from '@shoelace-style/shoelace/dist/components/divider/divider.js';
+import SlSelect from '@shoelace-style/shoelace/dist/components/select/select.js';
+import SlIcon from '@shoelace-style/shoelace/dist/components/icon/icon.js';
+import SlMenu from '@shoelace-style/shoelace/dist/components/menu/menu.js';
+import SlMenuItem from '@shoelace-style/shoelace/dist/components/menu-item/menu-item.js';
+import SlSwitch from '@shoelace-style/shoelace/dist/components/switch/switch.js';
+import SlOption from '@shoelace-style/shoelace/dist/components/option/option.js';
 import LockMarker from './CodeMirror/LockMarker';
 import CustomGutter from './CodeMirror/CustomGutter';
 // import { loadPyodide } from 'pyodide';
 
 export type LanguageModule = {
-  name: string
-  executionFunction: (code: string, context: Code) => any
-  languageExtension: LanguageSupport
-}
+    name: string;
+    executionFunction: (code: string, context: Code) => any;
+    languageExtension: LanguageSupport;
+};
 
 @customElement('ww-code')
 export default class Code extends LitElementWw {
     static styles = style;
 
     static JSONConverter = {
-      fromAttribute: (value: string) => {
-        return JSON.parse(value);
-      },
-      toAttribute: (value: Array<number>) => {
-        return JSON.stringify(value);
-      },
+        fromAttribute: (value: string) => {
+            return JSON.parse(value);
+        },
+        toAttribute: (value: Array<number>) => {
+            return JSON.stringify(value);
+        },
     };
 
     static languages: LanguageModule[] = [javascriptModule, htmlModule, cssModule];
     codeMirror: EditorView = new EditorView();
 
-    @property({ type: Array, attribute: true, reflect: true})
+    @property({ type: Array, attribute: true, reflect: true })
     lockedLines: number[] = [];
 
-    @property({ type: Boolean, attribute: true, reflect: true})
-    runnable = true
+    @property({ type: Boolean, attribute: true, reflect: true })
+    runnable = true;
 
-    @property({ type: Boolean, attribute: true, reflect: true})
-    autocomplete = false
+    @property({ type: Boolean, attribute: true, reflect: true })
+    autocomplete = false;
 
-    @property({type: Boolean, attribute: true, reflect: true})
-    hideExecutionTime = true
+    @property({ type: Boolean, attribute: true, reflect: true })
+    hideExecutionTime = true;
 
-    @property({type: Boolean, attribute: true, reflect: true})
-    hideExecutionCount = true
+    @property({ type: Boolean, attribute: true, reflect: true })
+    hideExecutionCount = true;
 
     @property({ attribute: true, reflect: true })
     code = this.codeMirror.state.doc.toString();
@@ -74,24 +75,31 @@ export default class Code extends LitElementWw {
     @property({ type: Number, attribute: true, reflect: true })
     executionCount = 0;
 
+    @property({ type: Boolean, attribute: true, reflect: true })
+    globalExecution = true;
+
     private disabledLines: Array<number> = [];
 
     @property({ attribute: true, reflect: true })
     get languageName() {
-      return this.languageModule.name
+        return this.languageModule.name;
     }
 
-    @property({attribute: false})
-    languageModule: LanguageModule = Code.languages[0]
+    set languageName(value: string) {
+        this.languageModule = Code.languages.find((l) => l.name === value) || Code.languages[0];
+    }
+
+    @property({ attribute: false })
+    languageModule: LanguageModule = Code.languages[0];
 
     private get codeRunner() {
-      return this.languageModule.executionFunction
+        return this.languageModule.executionFunction;
     }
 
-    @property({attribute: true, reflect: true, converter: Code.JSONConverter})
-    results: string[] = []
+    @property({ type: Array, attribute: true, reflect: true, converter: Code.JSONConverter })
+    results: Array<{ text: string; color: string } | undefined> = [];
 
-    @property({attribute: false})
+    @property({ attribute: false })
     executionTime: number = 0;
 
     @query('#iframePreview')
@@ -108,85 +116,98 @@ export default class Code extends LitElementWw {
     );
 
     static get scopedElements() {
-      return {
-        'sl-checkbox': SlCheckbox,
-        'sl-select': SlSelect,
-        'sl-menu': SlMenu,
-        'sl-menu-item': SlMenuItem,
-        'sl-button': SlButton,
-        'sl-divider': SlDivider,
-        'sl-card': SlCard,
-        'sl-switch': SlSwitch,
-        'sl-icon': SlIcon,
-      };
+        return {
+            'sl-checkbox': SlCheckbox,
+            'sl-select': SlSelect,
+            'sl-menu': SlMenu,
+            'sl-menu-item': SlMenuItem,
+            'sl-button': SlButton,
+            'sl-divider': SlDivider,
+            'sl-card': SlCard,
+            'sl-switch': SlSwitch,
+            'sl-icon': SlIcon,
+
+            'sl-option': SlOption,
+        };
     }
 
     focus() {
-      this.codeMirror?.focus();
+        this.codeMirror?.focus();
     }
 
-    @query("pre")
-    pre?: HTMLPreElement
+    @query('pre')
+    pre?: HTMLPreElement;
 
     firstUpdated() {
-      this.codeMirror = this.createCodeMirror(this.pre);
-      this.codeMirror.focus();
-      if (this.iframePreview) {
-        this.iframePreview.addEventListener('load', () => {
-          if (this.iframePreview && this.iframePreview.contentWindow) {
-            this.iframePreview.height = this.iframePreview.contentWindow.document.body.scrollHeight + 16 + 'px';
-          }
-        });
-      }
-      for (const line of this.lockedLines) {
-        this.makeLineReadOnly(line);
-      }
+        this.codeMirror = this.createCodeMirror(this.pre);
+        this.codeMirror.focus();
+        if (this.iframePreview) {
+            this.iframePreview.addEventListener('load', () => {
+                if (this.iframePreview && this.iframePreview.contentWindow) {
+                    this.iframePreview.height = this.iframePreview.contentWindow.document.body.scrollHeight + 16 + 'px';
+                }
+            });
+        }
+        for (const line of this.lockedLines) {
+            this.makeLineReadOnly(line);
+        }
+    }
+
+    protected updated(_changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>): void {
+        if (_changedProperties.has('autocomplete')) {
+            this.setAutocompletion(this.autocomplete);
+        }
+
+        if (_changedProperties.has('code')) {
+            if (this.codeMirror.state.doc.toString() !== this.code) {
+                this.codeMirror.dispatch({
+                    changes: {
+                        from: 0,
+                        to: undefined,
+                        insert: this.code,
+                    },
+                });
+            }
+        }
     }
 
     render() {
-      return html`
-        <style>${style}</style>
-        ${this.Code()}
-        ${this.Footer()}
-        ${this.Output()}
-        ${this.Options()}
-      `
-  }
+        return html`
+            <style>
+                ${style}
+            </style>
+            ${this.Code()} ${this.Footer()} ${this.Output()} ${this.editable ? this.Options() : ''}
+        `;
+    }
 
     Code() {
-      return html`<pre></pre>`
+        return html`<pre></pre>`;
     }
 
     Footer() {
-      return html`<footer>
-        <sl-button
-          @click=${this.runCode}
-          ?disabled=${this.codeRunner === undefined}
-          ><sl-icon name="caret-right"></sl-icon> Run
-          ${this.hideExecutionCount ? '' : `(${this.executionCount})`}
-        </sl-button>
-        <sl-button
-            @click=${() => {
-              this.results = [];
-              this.executionTime = 0;
-              this.codeMirror.focus();
-            }}
-            >Clear Output</sl-button
-        >
-        <sl-select ?disabled=${!this.canChangeLanguage}>
-          ${Code.languages.map(l => html`
-            <sl-option @click=${() => this.changeLanguage(l)}>
-              ${l.name}
-            </sl-option>`
-          )}
-        </sl-select>
-      </footer>`
+        return html`<footer>
+            <sl-button @click=${this.runCode} ?disabled=${this.codeRunner === undefined}
+                ><sl-icon name="caret-right"></sl-icon>${this.globalExecution ? '' : 'isolated'} Run
+                ${this.hideExecutionCount ? '' : `(${this.executionCount})`}
+            </sl-button>
+            <sl-button
+                @click=${() => {
+                    this.results = [];
+                    this.executionTime = 0;
+                    this.codeMirror.focus();
+                }}
+                >Clear Output</sl-button
+            >
+            <sl-select ?disabled=${!this.canChangeLanguage}>
+                ${Code.languages.map(
+                    (l) => html` <sl-option @click=${() => this.changeLanguage(l)}> ${l.name} </sl-option>`
+                )}
+            </sl-select>
+        </footer>`;
     }
 
     Output() {
-      return html`<output>
-        ${this.Result()}
-      </output>`
+        return html`<output> ${this.Result()} </output>`;
     }
 
     Options() {
@@ -201,6 +222,16 @@ export default class Code extends LitElementWw {
                 }}
                 ?checked=${this.runnable}
                 >Allow Code execution</sl-switch
+            >
+            <sl-switch
+                @sl-change=${(event: any) => {
+                    if (event.target) {
+                        let target = event.target as SlSwitch;
+                        this.globalExecution = target.checked;
+                    }
+                }}
+                ?checked=${this.globalExecution}
+                >Global execution</sl-switch
             >
             <sl-switch
                 @sl-change=${(event: any) => {
@@ -223,12 +254,12 @@ export default class Code extends LitElementWw {
                 >Autocompletion</sl-switch
             >
             <sl-switch
-                @sl-change=${(e: any) => this.hideExecutionTime = !e.target.checked}
+                @sl-change=${(e: any) => (this.hideExecutionTime = !e.target.checked)}
                 ?checked=${!this.hideExecutionTime}
                 >Show execution time</sl-switch
             >
             <sl-switch
-                @sl-change=${(e: any) => this.hideExecutionCount = !e.target.checked}
+                @sl-change=${(e: any) => (this.hideExecutionCount = !e.target.checked)}
                 ?checked=${!this.hideExecutionCount}
                 >Show execution count</sl-switch
             >
@@ -237,15 +268,13 @@ export default class Code extends LitElementWw {
 
     Result() {
         switch (this.languageName) {
-            case 'JS': case 'Python':
-              const outputs = this.results.filter(r => r !== undefined).map(r => html`<code>${r}</code>`)
-              return html`
-                <div class="outputs">
-                  ${outputs}
-                </div>
-                <div class="executionTime">
-                    ${this.executionTime.toFixed(1)}ms
-                </div>`
+            case 'JS':
+            case 'Python':
+                const outputs = this.results
+                    .filter((r) => r !== undefined)
+                    .map((r) => html`<code style="color:${r?.color}">${r?.text}</code>`);
+                return html` <div class="outputs">${outputs}</div>
+                    <div class="executionTime">${this.executionTime.toFixed(1)}ms</div>`;
             case 'HTML':
                 return html` <iframe id="iframePreview" class="htmlPreview" srcdoc=${this.results[0]}></iframe>`;
             case 'CSS':
@@ -268,13 +297,14 @@ export default class Code extends LitElementWw {
         if (!this.codeRunner) {
             return;
         }
-        this.results = []
+
+        this.executionCount++;
+        this.results = [];
         const code = this.codeMirror.state.doc.toString();
         const startTime = performance.now();
         await this.codeRunner(code, this);
         const endTime = performance.now();
         this.executionTime = endTime - startTime;
-        this.executionCount++;
     }
 
     private changeLanguage(language: any) {
